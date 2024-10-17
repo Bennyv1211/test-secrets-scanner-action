75973eefedd49f0a2b3bbf263fe2a7dfb53d6d08
Benny Vermeulen <108191505+Bennyv1211@users.noreply.github.com>
2024-10-17 18:24:58 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+// Dummy secrets for testing purposes
+const apiKey = "AIzaSyD-abc12345dummy-secret-key";
+const accessToken = "ghp_12345ABCfakeTokenGoesHere";
+const databasePassword = "dummy_db_password_67890";
+
+function connectToDatabase() {
+    console.log(`Connecting to the database using password: ${databasePassword}`);
+}
