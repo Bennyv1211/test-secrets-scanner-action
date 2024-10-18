044d0f598ee2daacc6ab9bb61087c5a7eb3df9c3
Benny Vermeulen <108191505+Bennyv1211@users.noreply.github.com>
2024-10-18 07:59:45 -0400

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
 # Credentials for testing purposes
 API_KEY = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
-SECRET_TOKEN = "test_secret_token_12345"
+SECRET_TOKEN = "test_secret_tovc ken_12345"
 DATABASE_PASSWORD = "DummyPassword_54321"
